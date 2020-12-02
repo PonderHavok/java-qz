d8083401c66f0272dbfc2be17a526b6088ceb5bf
Jonny Talbott <transverseponderer@gmail.com>
2020-12-02 14:39:47 -0500

    Adding JS folder and files

---
+++ js/q&a.js
@@ -0,0 +1,28 @@
+var questions = [
+  {
+    title: "Which of these is not used to loop?",
+    choices: ["foreach", "sequence", "for", "while"],
+    answer: "sequence",
+  },
+  {
+    title:
+      "JS date function starts in seconds to current day from what day in 1970",
+    choices: ["June 1", "December 31", "April 23", "January 1"],
+    answer: "January 1",
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["curly brackets", "quotes", "parentheses", "square brackets"],
+    answer: "parentheses",
+  },
+  {
+    title: "Which of these is not a way to save a variable",
+    choices: ["var", "const", "let", "vet"],
+    answer: "vet",
+  },
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["alerts", "booleans", "numbers", "strings"],
+    answer: "alerts",
+  },
+];
